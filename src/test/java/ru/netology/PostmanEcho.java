77385063f5034c9c3d1de3a73bdef9416a9cdc6e
Ekaterina Shilova <ekaterinalstmpr@gmail.com>
2021-12-02 12:38:43 +0300

    reformat code

--- src/test/java/ru/netology/PostmanEcho.java
+++ src/test/java/ru/netology/PostmanEcho.java
@@ -19,7 +19,7 @@ public class PostmanEcho {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("some data"))
         ;
     }
 
