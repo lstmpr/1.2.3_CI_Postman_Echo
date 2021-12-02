b1c37f4546fb0aa49a7c5ddb64dd639761d50fd7
Ekaterina Shilova <ekaterinalstmpr@gmail.com>
2021-12-02 12:43:12 +0300

    changes test

--- src/test/java/ru/netology/PostmanEcho.java
+++ src/test/java/ru/netology/PostmanEcho.java
@@ -13,7 +13,6 @@ public class PostmanEcho {
     void shouldPost() {
         given()
                 .baseUri("https://postman-echo.com")
-                .contentType("text/plain; charset=UTF-8")
                 .body("some data")
                 .when()
                 .post("/post")
