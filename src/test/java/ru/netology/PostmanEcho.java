53a51fe84dd2ec877c5083e4abe9004f034f1c81
Ekaterina Shilova <ekaterinalstmpr@gmail.com>
2021-12-02 12:49:10 +0300

    break test

--- src/test/java/ru/netology/PostmanEcho.java
+++ src/test/java/ru/netology/PostmanEcho.java
@@ -18,7 +18,7 @@ public class PostmanEcho {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("some value"))
         ;
     }
 
