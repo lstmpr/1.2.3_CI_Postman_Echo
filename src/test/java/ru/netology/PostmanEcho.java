d0da5e3d9417b3e122852d4c6585cc26ab08f0a5
Ekaterina Shilova <ekaterinalstmpr@gmail.com>
2021-12-02 12:32:36 +0300

    add files

---
+++ src/test/java/ru/netology/PostmanEcho.java
@@ -0,0 +1,25 @@
+package ru.netology;
+
+import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+
+
+public class PostmanEcho {
+    @Test
+    void shouldPost() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some value"))
+                ;
+    }
+
+}
